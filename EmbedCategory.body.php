7e8536c8b21ccaabe4244734ca0902412a909c05
Chris <chris@starforge.co.uk>
2019-09-15 12:53:44 +0100

    Add support for exclusion and fix style issues

--- EmbedCategory.body.php
+++ EmbedCategory.body.php
@@ -36,106 +36,178 @@ class EmbedCategory {
 	 */
 	public static function onParserFirstCallInit( &$parser ) {
 		// Adds the <embedcategory>...</embedcategory> tag to the parser.
-		$parser -> setHook( 'embedcategory', 'EmbedCategory::parserHook' );
+		$parser->setHook( 'embedcategory', 'EmbedCategory::parserHook' );
 
 		return true;
 	}
+
+
+	/**
+	 * A convenience function to generate an embedcategory error message
+	 * <div> element.
+	 *
+	 * @param string $message The error message to show in the div.
+	 * @return string A HTML element containing the error message.
+	 */
+	public static function errorDiv( $message ) {
+
+		return Html::rawelement( 'div',
+			array( 'class' => 'embedcategory error' ),
+			$message );
+	}
+
+
+	/**
+	 * A convenience function to generate a string containing a HTML link with
+	 * bold link text.
+	 *
+	 * @param string $link The URL to set as the link href.
+	 * @param string $text The text to show as the link.
+	 * @return string A string containing the HTML <a> element.
+	 */
+	public static function strongLink( $link, $text ) {
+
+		return Html::rawElement( 'a',
+			array( 'href' => $link ),
+			Html::element( 'strong',
+				[],
+				$text
+			)
+		);
+	}
+
+
+	/**
+	 * Given the body of an embedcategory tag, generate an array of
+	 * page titles to ignore.
+	 *
+	 * @todo Potential enhancement would be to ignore lines that
+	 *       are not valid titles. Not sure if it's worth the
+	 *       time and overhead, though.
+	 *
+	 * @param string body The body of the embedcategory tag
+	 * @return array An array of page titles to ignore.
+	 */
+	public static function buildExcludeList( $body ) {
+
+		$lines = explode( "\n", $body );
+
+		# TODO: check lines are valid titles?
+
+		return $lines;
+	}
+
 
 	/**
 	 * Generate a string of HTML containing links to pages in a category,
 	 * optionally including a 'more' link at the end.
 	 *
 	 * @param string $name The name of the category, not including the
-	 *                     Category: namespace
+	 *					   Category: namespace
 	 * @param integer $limit The number of pages to link to.
 	 * @param boolean $showMore If there are more pages in the category than
-	 *                          $limit, and this is true, a link to the
-	 *                          category is added to the list of links.
+	 *							$limit, and this is true, a link to the
+	 *							category is added to the list of links.
+	 * @param array $exclude An
 	 * @return string The HTML containing the page links.
 	 */
-	public static function buildCategoryList( $name, $limit = false, $showMore = true ) {
+	public static function buildCategoryList( $name, $limit = false, $showMore = true, $exclude = array() ) {
 		global $wgEmbedCategoryLinkEmpty;
 		$result = "";
 
 		// Fetch the category information
 		$category = Category::newFromName( $name );
 		if( !$category ) {
-			return Html::rawelement( 'div',
-									 array( 'class' => 'categorylist error' ),
-									 wfMessage( 'embedcategory-bad' ) );
+			return self::errorDiv( wfMessage( 'embedcategory-bad' ) );
 		}
 
 		// If there are no pages, and an empty cateogry link should be
 		// generated, do so.
-		if( $wgEmbedCategoryLinkEmpty && !$category -> getPageCount() ) {
-			$catTitle = $category -> getTitle();
+		if( $wgEmbedCategoryLinkEmpty && !$category->getPageCount() ) {
+			$catTitle = $category->getTitle();
 
-			return Html::rawElement( 'div', [],
-									 wfMessage( 'embedcategory-empty' ) .
-									 Html::rawElement( 'a',
-													   array( 'href' => $catTitle -> getLinkURL()),
-													   Html::element( 'strong',
-																	  [],
-																	  $catTitle -> getText())
-									 )
+			return Html::rawElement( 'div',
+				[],
+				wfMessage( 'embedcategory-empty' ) .
+					self::strongLink( $catTitle->getLinkURL(),
+						$catTitle->getText()
+					)
 			);
 		}
 
 		// Convert the list of category members to a string of HTML elements
-		$members  = $category -> getMembers( $limit );
-		while( $members -> valid() ) {
-			$member = $members -> current();
+		$members  = $category->getMembers( $limit );
+		while( $members->valid() ) {
+			$member = $members->current();
 
-			// TODO: Is <li> the best option here?
-			$result .= Html::rawElement( 'li', [],
-										 Html::element( 'a',
-														array( 'href' => $member -> getLinkURL() ),
-														$member -> getText() )
-			);
+			// Ignore pages if they are in the ignore list.
+			if( !in_array( $member->getText(), $exclude, true ) ) {
+				$result .= Html::rawElement( 'li',
+					[],
+					Html::element( 'a',
+						array( 'href' => $member->getLinkURL() ),
+						$member->getText()
+					)
+				);
+			}
 
-			$members -> next();
+			$members->next();
 		}
 
-		// If there are more pages in the categroy than the limit, and the
+		// If there are more pages in the category than the limit, and the
 		// 'show more' option is enabled, output a 'More' link at the end.
-		if( $showMore && $limit && $category -> getPageCount() > $limit ) {
-			$catTitle = $category -> getTitle();
+		if( $showMore && $limit && $category->getPageCount() > $limit ) {
+			$catTitle = $category->getTitle();
 
-			$result .= Html::rawElement( 'li', [],
-										 Html::rawElement( 'a',
-														   array( 'href' => $catTitle -> getLinkURL()),
-														   Html::element( 'strong',
-																		  [],
-																		  wfMessage( 'embedcategory-more' ))
-										 )
-			);
+			$result .= Html::rawElement( 'li',
+				[],
+				self::strongLink( $catTitle->getLinkURL(),
+					wfMessage( 'embedcategory-more' )
+				)
+ 			);
 		}
 
-		return Html::rawElement( 'ul', [ 'class' => 'category_members' ], $result );
+		return Html::rawelement( 'div',
+			array( 'class' => 'categorylist' ),
+			Html::rawElement( 'ul',
+				[ 'class' => 'category_members' ],
+				$result )
+		);
 	}
 
-  	/**
+
+	/**
 	 * Parser hook handler for <embedcategory>
 	 *
-	 * @param string $input  The content of the tag.
-	 * @param array  $args   The attributes of the tag.
+	 * @param string $input	 The content of the tag.
+	 * @param array	 $args	 The attributes of the tag.
 	 * @param Parser $parser Parser instance available to render wikitext into html,
-	 *                       or parser methods.
+	 *						 or parser methods.
 	 * @param PPFrame $frame Can be used to see what template arguments ({{{1}}})
-	 *                       this hook was used with.
+	 *						 this hook was used with.
 	 * @return string HTML to insert in the page.
 	 */
 	public static function parserHook( $input, $args = array(), $parser, $frame ) {
 		global $wgOut;
 
+		// Only generate the list if the category is specified
 		if($args['category']) {
-			return Html::rawelement( 'div',
-									 array( 'class' => 'categorylist' ),
-									 self::buildCategoryList( $args['category'], $args['limit'], $args['showmore'] ) );
+			// Obtain the body of the tag (with template expansions) and
+			// convert it to an exclude list
+			$body    = $parser->recursiveTagParse( $input, $frame );
+			$exclude = self::buildExcludeList($body);
+
+			if($args['columns']) {
+
+			} else {
+				return self::buildCategoryList( $args['category'],
+					$args['limit'],
+					$args['showmore'],
+					$exclude
+				);
+			}
 		} else {
-			return Html::rawelement( 'div',
-									 array( 'class' => 'categorylist error' ),
-									 wfMessage( 'embedcategory-none' ) );
+			return self::errorDiv( wfMessage( 'embedcategory-none' ) );
 		}
 	}
 }
